44797a2d484d451a2f8aff736c5a197ac07c7804
Jakub <devcobbier@gmail.com>
2022-06-15 19:22:34 +0200

    Added sidebar for desktopn and mobile sizes, and added experience tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackgroundComponent } from './components/background/background.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ExperienceTabComponent } from './components/sidebar/tabs/experience-tab/experience-tab.component';
 
 @NgModule({
-  declarations: [AppComponent, BackgroundComponent, SidebarComponent],
+  declarations: [AppComponent, BackgroundComponent, SidebarComponent, ExperienceTabComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/sidebar/tabs/experience-tab/experience-tab.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+
+export namespace NContent {
+  export interface ContentData {
+    title: string;
+    chartLinesLength: number;
+    companies: CompanyData[];
+  }
+
+  export interface CompanyData {
+    id: number;
+    name: string;
+    city: string;
+    markerPosition?: EMarkerPosition;
+    skills: SkillData[];
+    exist: boolean;
+  }
+
+  export interface SkillData {
+    name: string;
+    img: string;
+  }
+}
+
+enum EMarkerPosition {
+  First = 0,
+  Second = 1,
+  Third = 2,
+  Fourth = 3,
+  Fifth = 4,
+  Sixth = 5,
+  Seventh = 6,
+  Eighth = 7,
+  Ninth = 8,
+  Tenth = 9,
+}
+
+@Component({
+  selector: 'app-experience-tab',
+  templateUrl: './experience-tab.component.html',
+  styleUrls: ['./experience-tab.component.scss'],
+})
+export class ExperienceTabComponent implements OnInit {
+  positions = EMarkerPosition;
+  content: NContent.ContentData = {
+    title: 'experience',
+    chartLinesLength: 10,
+    companies: [
+      {
+        id: 0,
+        name: 'Selea',
+        city: 'Ruda Śląska',
+        markerPosition: this.positions.First,
+        skills: [
+          { name: 'WordPress', img: 'wordpress' },
+          { name: 'HTML5', img: 'html' },
+        ],
+        exist: true,
+      },
+      {
+        id: 1,
+        name: 'Tom&Co',
+        markerPosition: this.positions.Second,
+        city: 'Katowice',
+        exist: true,
+        skills: [
+          { name: 'HTML5', img: 'html' },
+          { name: 'SCSS', img: 'sass' },
+          { name: 'Typescript', img: 'typescript' },
+          { name: 'Angular', img: 'angular' },
+        ],
+      },
+    ],
+  } as NContent.ContentData;
+  companyData: NContent.CompanyData = this.content.companies[1];
+
+  constructor() {
+    let length = this.content.chartLinesLength - this.content.companies.length;
+    for (let i = 0; i < length; i++) {
+      this.content.companies.push({
+        id: i + 2,
+        name: '',
+        city: '',
+        exist: false,
+        markerPosition: undefined,
+        skills: [],
+      });
+    }
+  }
+
+  ngOnInit(): void {}
+
+  get markerPosition() {
+    let positions = ['first-company', 'second-company'];
+
+    return this.companyData.markerPosition
+      ? positions[this.companyData.markerPosition]
+      : positions[0];
+  }
+
+  showCompanyData(data: NContent.CompanyData) {
+    if (data.name && data.city) {
+      //Company Exists
+      this.companyData = this.content.companies[data.id];
+    }
+  }
+}
