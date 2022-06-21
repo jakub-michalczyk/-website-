4a134ae3fa6075b62a2f3bc6dc98a7be997fb186
Jakub <devcobbier@gmail.com>
2022-06-21 19:15:44 +0200

    Added placeholder before image is loaded in contact tab

--- src/app/components/sidebar/tabs/contact-tab/contact-tab.component.ts
+++ src/app/components/sidebar/tabs/contact-tab/contact-tab.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ContactTabComponent implements OnInit {
   contactForm = {} as FormGroup;
   isSubmit = false;
+  showPlaceholder = true;
   submitMessage = '';
 
   constructor(
@@ -38,6 +39,7 @@ export class ContactTabComponent implements OnInit {
 
   loaded(evt: Event) {
     let img = evt.target as HTMLImageElement;
+    this.showPlaceholder = false;
     img.removeAttribute('hidden');
   }
 }
