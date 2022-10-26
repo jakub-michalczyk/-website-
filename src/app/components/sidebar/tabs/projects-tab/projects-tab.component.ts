13b23558ebef504ab7ecef205f0277d28d20fda7
Jakub <devcobbier@gmail.com>
2022-10-26 20:33:03 +0200

    Added guess this artist project to projects tab, and changed some styling of projects container

--- src/app/components/sidebar/tabs/projects-tab/projects-tab.component.ts
+++ src/app/components/sidebar/tabs/projects-tab/projects-tab.component.ts
@@ -1,12 +1,32 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  AfterViewInit,
+} from '@angular/core';
+
+interface Projects {
+  name: string;
+  link?: string;
+}
 
 @Component({
   selector: 'app-projects-tab',
   templateUrl: './projects-tab.component.html',
   styleUrls: ['./projects-tab.component.scss'],
 })
-export class ProjectsTabComponent implements OnInit {
+export class ProjectsTabComponent implements OnInit, AfterViewInit {
   loaded = false;
+  projects: Projects[] = [
+    {
+      name: 'guess-this-artist',
+      link: 'https://guess-this-artist.netlify.app/start',
+    },
+    { name: 'website', link: 'https://jakubmichalczyk.netlify.app/' },
+    { name: 'video-finder', link: 'https://video-finder.netlify.app/' },
+    { name: '', link: '' },
+  ];
+
   constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {}
@@ -16,5 +36,11 @@ export class ProjectsTabComponent implements OnInit {
       this.loaded = true;
       this.cdr.detectChanges();
     }, 500);
+  }
+
+  getImage(img: string) {
+    return img
+      ? '/assets/images/' + img + '.jpg'
+      : '/assets/images/placeholder.jpg';
   }
 }
